f473de3e620d0b1eb1543a51d852c872884bb76c
RussianBlue25 <40377001+RussianBlue25@users.noreply.github.com>
2021-01-24 21:30:51 +0900

    Update kat.rb

--- Formula/kat.rb
+++ Formula/kat.rb
@@ -4,8 +4,8 @@
 class Kat < Formula
   desc "このリポジトリは先輩の偉業を称える内輪ネタです。なんの役にも立ちません"
   homepage ""
-  url "https://github.com/RussianBlue25/kat/releases/download/0.0.1/kat"
-  sha256 "3f4090dd8c807d74030d89508870cac3345f011d31b81ccc2cc22cc51bfa2cb8"
+  url "https://github.com/RussianBlue25/kat/releases/download/0.0.2/kat"
+  sha256 "1fd2dc7aa76202fda34211f61cfc3045ece24f2fbb7b77355abb0c53440bcdb0"
 
   def install
     bin.install "kat"
